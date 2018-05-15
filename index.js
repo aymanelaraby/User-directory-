0045bd85c84117bd76ea9c6f5ce04e8dcf0ab002
aymanelaraby <memoman1133@gmail.com>
2018-05-14 22:26:01 -0500

    form

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const button = document.querySelector('button')
+const button = document.querySelector('#button1')
+const paragraph = document.querySelector('.greeting')
 
 console.log(button)
 function changeText(){
 
-    const paragraph = document.querySelector('.greeting')
+   
     paragraph.textContent ='howedy' 
 }
 
 button.addEventListener('click', changeText)
+
+ 
+const buttonx = document.querySelector('#button2')
+
+let y = document.getElementById('input')
+
+function x(){
+   
+    paragraph.textContent = y
+}
+
+if(y){
+    buttonx.addEventListener('click', x)
+}
